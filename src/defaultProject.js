d6a57c6a2d52a1e58075f79b99c0f67d630f7031
alaugustusembrace <alaugustuscayacap@gmail.com>
2025-10-12 20:19:58 +0800

    add projectDivision event listener if clicked it clears the listArea then assigns its own lists

--- src/defaultProject.js
+++ src/defaultProject.js
@@ -1,4 +1,13 @@
-const createDefaultProject = (title, description) => {
+import { createDefaultTasks } from "./defaultTasks.js";
+
+const defaultTasks = createDefaultTasks;
+
+const createDefaultProject = (
+  title,
+  description,
+  listArea,
+  createDefaultTasks
+) => {
   const projectDivision = document.createElement("button");
   projectDivision.classList.add("projectDivision");
   const projectTitle = document.createElement("h2");
@@ -7,6 +16,12 @@ const createDefaultProject = (title, description) => {
   projectDescription.textContent = description;
   projectDivision.appendChild(projectTitle);
   projectDivision.appendChild(projectDescription);
+
+  // Clearing the listArea then Assigning its respective lists
+  projectDivision.addEventListener("click", () => {
+    listArea.innerHTML = "";
+    defaultTasks(listArea);
+  });
 
   return { projectDivision };
 };
