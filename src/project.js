d869258ae130bb545b72db96e7b25c911571d999
alaugustusembrace <alaugustuscayacap@gmail.com>
2025-10-15 20:53:34 +0800

    add dueDate, and priority as properties to the tasks property of the Project class

--- src/project.js
+++ src/project.js
@@ -4,18 +4,40 @@ class Project {
     this.description = description;
     this.tasks = isDefault
       ? [
-          { id: 1, title: "Study", description: "Study properly for 1 hour" },
-          { id: 2, title: "Sleep", description: "Sleep soundly for 8 hours" },
-          { id: 3, title: "Eat", description: "Eat real food 3 times a day" },
+          {
+            id: 1,
+            title: "Study",
+            description: "Study properly for 1 hour",
+            dueDate: "10/25/25",
+            priority: 1,
+          },
+          {
+            id: 2,
+            title: "Sleep",
+            description: "Sleep soundly for 8 hours",
+            dueDate: "10/25/25",
+            priority: 1,
+          },
+          {
+            id: 3,
+            title: "Eat",
+            description: "Eat real food 3 times a day",
+            dueDate: "10/25/25",
+            priority: 2,
+          },
           {
             id: 4,
             title: "Exercise",
             description: "Exercise atleast 30 minutes",
+            dueDate: "10/25/25",
+            priority: 2,
           },
           {
             id: 5,
             title: "Run",
             description: "Run 1 kilometer early in the morning",
+            dueDate: "10/25/25",
+            priority: 3,
           },
         ]
       : [];
