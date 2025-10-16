16ee93afe449d47161a4ee7c7f7f4db70010da25
alaugustusembrace <alaugustuscayacap@gmail.com>
2025-10-16 19:20:41 +0800

    add description, dueDate, and priority for default tasks, but not yet done

--- src/defaultTasks.js
+++ src/defaultTasks.js
@@ -9,6 +9,31 @@ const createDefaultTasks = (listArea) => {
     const taskContainer = document.createElement("div");
     taskContainer.classList.add("taskContainer");
 
+    const taskWrapper = document.createElement("div");
+    taskWrapper.classList.add("taskWrapper");
+
+    const taskTitleAndDescWrapper = document.createElement("div");
+    taskTitleAndDescWrapper.classList.add("taskTitleAndDescWrapper");
+
+    const taskTitle = document.createElement("h4");
+    taskTitle.classList.add("taskTitle");
+    taskTitle.textContent = task.title;
+
+    const taskDescription = document.createElement("p");
+    taskDescription.classList.add("taskDescription");
+    taskDescription.textContent = task.description;
+
+    const taskDateAndPriorityWrapper = document.createElement("div");
+    taskDateAndPriorityWrapper.classList.add("taskDateAndPriorityWrapper");
+
+    const taskDate = document.createElement("p");
+    taskDate.classList.add("taskDate");
+    taskDate.textContent = task.dueDate;
+
+    const taskPriority = document.createElement("p");
+    taskDate.classList.add("taskPriority");
+    taskPriority.textContent = task.priority;
+
     const taskBtnWrapper = document.createElement("div");
     taskBtnWrapper.classList.add("taskBtnWrapper");
 
@@ -20,10 +45,14 @@ const createDefaultTasks = (listArea) => {
     removeTaskBtn.classList.add("remove-btn");
     removeTaskBtn.textContent = "X";
 
+    taskTitleAndDescWrapper.append(taskTitle, taskDescription);
+    taskDateAndPriorityWrapper.append(taskDate, taskPriority);
+
+    taskWrapper.append(taskTitleAndDescWrapper, taskDateAndPriorityWrapper);
+
     taskBtnWrapper.append(checkTaskBtn, removeTaskBtn);
 
-    taskContainer.textContent = task.title;
-    taskContainer.appendChild(taskBtnWrapper);
+    taskContainer.append(taskWrapper, taskBtnWrapper);
 
     taskItem.appendChild(taskContainer);
 
